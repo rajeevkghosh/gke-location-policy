1770aa01d393572bec7f0cd0880e742afead993b
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-29 13:16:32 +0530

    hi

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ provider "google" {
 
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
-  location = "us-central1"
+  #location = "us-central1"
+  location = "US"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
@@ -29,7 +30,8 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
-  location   = "us-central1"
+  #location   = "us-central1"
+  location   = "US"
   cluster    = google_container_cluster.primary.name
   node_count = 1
 
