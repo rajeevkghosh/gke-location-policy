29ca051388631796bdd981ab6f118fe8202f07fe
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-29 13:53:44 +0530

    updated

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
-  location = "us-central1"
+  #location = "us-central1"
   #location = "US"
 
   # We can't create a cluster with no node pool defined, but we want to only use
@@ -31,7 +31,7 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
   #location   = "us-central1"
-  location   = "US"
+  #location   = "US"
   cluster    = google_container_cluster.primary.name
   node_count = 1
 
